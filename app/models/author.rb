116baa348fe6522d48bf0c1f06a98d4618266945
Mulu <brianmulu007@gmail.com>
2022-11-22 14:12:21 +0300

    Done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+    # all authors have a name and no name can repeated
+    validates :name, presence: true, uniqueness: true
+    # number is exactly 10 digits
+    validates :phone_number, length: {is: 10}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    # validate is used for custom error messages
+    validate :title_clickbait
+
+    def title_clickbait
+        # \d+ -> one or more digits eg.Top 234
+        # if the title does not match the words the brackets throw the error
+        unless self.title && self.title.match(/Won't Believe | Secret |Top \d+| Guess/)
+            errors.add(:title, "is not sufficiently clickbait-y")
+        end
+    end
 end
